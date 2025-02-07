0bdadf35582cd5713b436044780ece3518241fb7
halong <halong30042009@gmail.com>
2025-02-07 20:49:58 +0700

    tiép tuc bài 5

---
+++ bai1/bai5.py
@@ -0,0 +1,21 @@
+#Toán tử học
+#+ : cộng
+#- : trừ
+#* : nhân
+#/ : chia thập phân
+#// : chia nguyên
+#% : chia dư
+#** : lũy thừa
+
+#Toán tử so sánh
+# == : ss bằng : 100 == 100
+# >  : lớn hơn : 400>300
+# >= : lớn hoặc bằng : 200>=100
+# <  : nhỏ hơn : 100<300
+# <= : nhỏ hoặc bằng : 300<=500
+# != : khác : 50!=20
+
+# Toán tử logic
+# and : toán tử và
+# or  : toán tử hoặc
+# not : toán tử phủ định
